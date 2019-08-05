02947baa631ddefff437335fed3e729de6aab180
Jason Aviles <sandman2k18@outlook.com>
2019-08-05 15:14:56 -0400

    create the route for post useing insert

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
 // implement your API here
 
 //imported express and the DB
-const express = require('express')
-const DB = require('./data/db')
+const express = require("express");
+const DB = require("./data/db");
 
 //used expressed and called it server
-const server = express()
+const server = express();
 
 // made express able to use json
-server.use(express.json())
+server.use(express.json());
 
 //made my first route to the root /
 
-server.get('/',(req,res)=>{
-    res.send('hello world')
-})
+server.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-server.get('/user',(req,res)=>{
-    DB.find().then(user =>{res.status(200).json(user)} ).catch(err =>{
-        res.status(500).json({message:'error cant get'})
-    } )
-})
+server.get("/user", (req, res) => {
+  DB.find()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "error cant get" });
+    });
+});
 
+server.post("/user", (req, res) => {
+  const UserInfo = req.body;
 
-const port = 8000
+  DB.insert(UserInfo)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "error cant get" });
+    });
+});
 
-server.listen(port, () => console.log(`api is running on port ${port}`))
+const port = 8000;
+
+server.listen(port, () => console.log(`api is running on port ${port}`));
