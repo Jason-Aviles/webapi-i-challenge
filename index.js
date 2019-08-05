1089fc5e3c4aa1524c176ae5a82cfe79fce9d2b1
Jason Aviles <sandman2k18@outlook.com>
2019-08-05 15:28:52 -0400

    made get get by id delete and edit routes

--- index.js
+++ index.js
@@ -22,9 +22,22 @@ server.get("/user", (req, res) => {
       res.status(200).json(user);
     })
     .catch(err => {
-      res.status(500).json({ message: "error cant get" });
+      res.status(500).json({  error: "The users information could not be retrieved." });
     });
 });
+
+
+server.get("/user/:id", (req, res) => {
+    const { id } = req.params;
+    DB. findById(id)
+      .then(userId => {
+        res.status(201).json(userId);
+      })
+      .catch(err => {
+        res.status(500).json({message: "The user with the specified ID does not exist."});
+      });
+  });
+
 
 server.post("/user", (req, res) => {
   const UserInfo = req.body;
@@ -34,10 +47,39 @@ server.post("/user", (req, res) => {
       res.status(201).json(user);
     })
     .catch(err => {
-      res.status(500).json({ message: "error cant get" });
+      res.status(500).json({ message: "error cant post" });
     });
 });
 
+
+server.delete("/user/:id", (req, res) => {
+  const { id } = req.params;
+  DB.remove(id)
+    .then(userId => {
+      res.status(201).json(userId);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "error cant post" });
+    });
+});
+
+
+
+
+
+server.put("/user/:id", (req,res)=>{
+    const { id } = req.params;
+    const UserInfo = req.body;
+    DB.update(id,UserInfo).then(
+        updated => {if(updated){res.status(200).json(updated)}}
+    ).catch(err =>{
+        res.status(500).json({message:'error cant post'})
+    } )
+})
+
+
+//created port
 const port = 8000;
 
+// listen to port
 server.listen(port, () => console.log(`api is running on port ${port}`));
