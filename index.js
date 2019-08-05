93d8ef516450c566d6b5a126861dd21a407bf2b9
Jason Aviles <sandman2k18@outlook.com>
2019-08-05 15:04:29 -0400

    made the get route  for /user

--- index.js
+++ index.js
@@ -10,9 +10,19 @@ const server = express()
 // made express able to use json
 server.use(express.json())
 
-//made my first route tothe root /
+//made my first route to the root /
 
 server.get('/',(req,res)=>{
     res.send('hello world')
 })
 
+server.get('/user',(req,res)=>{
+    DB.find().then(user =>{res.status(200).json(user)} ).catch(err =>{
+        res.status(500).json({message:'error cant get'})
+    } )
+})
+
+
+const port = 8000
+
+server.listen(port, () => console.log(`api is running on port ${port}`))
