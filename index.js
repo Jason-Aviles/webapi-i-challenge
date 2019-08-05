d2012e45860e6a2faeb47beefa7188ed737f68c7
Jason Aviles <sandman2k18@outlook.com>
2019-08-05 14:57:18 -0400

    made my first root to the root /

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 // implement your API here
+
+//imported express and the DB
 const express = require('express')
 const DB = require('./data/db')
+
+//used expressed and called it server
+const server = express()
+
+// made express able to use json
+server.use(express.json())
+
+//made my first route tothe root /
+
+server.get('/',(req,res)=>{
+    res.send('hello world')
+})
+
